9b650e0dbbece6df74d15bdc3d701da52ac59044
cecchetti <pedro.cecchetti@gmail.com>
2019-10-22 23:57:55 -0300

    feat: connects app to MONGODB Database

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose')
 
-const port = 3000
+
+const port = 3000;
 
 app.get('/', (req, res)=> res.send('Hello World'))
+
+// Connect to DB
+mongoose.connect(
+    'mongodb://root:example@localhost:27017/knights',
+    {useNewUrlParser: true},
+    ()=>{
+        console.log('Connected to DB!')
+    }
+)
 
 app.listen(port, () => console.log(`Example app running on port ${port}!`))
